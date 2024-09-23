b692070f07e06662a77ff8921c82cac2d98336c7
Pooja Sharma <sharmapooja4917@gmail.com>
2024-09-23 15:13:53 +0530

    .env file added

--- src/utils/fetchWeatherDataForCity.js
+++ src/utils/fetchWeatherDataForCity.js
@@ -1,9 +1,14 @@
 import axios from "axios"
+const apiKey = import.meta.env.VITE_API_KEY;
+const apiUrl = import.meta.env.VITE_API_URL;
 
-const url = `https://api.weatherapi.com/v1/forecast.json?key=9539e6bbda834b3789564112242109`
+const url = `${apiUrl}?key=${apiKey}`
+// console.log(url)
+
 
 export const fetchWeatherDataForCity = async (city) => {
    const response = await axios.get(`${url}&q=${city}&days=5`)
+   console.log(`${url}&q=${city}&days=5`)
    
    return await response.data
 }
